72c185d62be3d4cc1d7df9cc0cb8aaed44597b53
SharmaTushar1 <Tusharsharma2440160@gmail.com>
2023-03-11 11:51:26 +0530

    Add friend functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+ # had issues with signup so explicitly added destroy session
+devise_scope :user do  
+   get '/users/sign_out' => 'devise/sessions#destroy'     
+end
   resources :friends
   get 'home/about', as: :aboutUs
-  root to: 'home#index'
+  root 'home#index'
 end
